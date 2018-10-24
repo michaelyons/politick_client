377dbac3b8f78633a88413eaec6303f4e711b8d5
Michael J. Lyons <mikelyons7@me.com>
2018-10-23 21:10:51 -0600

    Add lobbyCard to cardcontainer render

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import LobbyCard from '../LobbyCard';
+import LobbyCard from '../LobbyCard';
 
-const CardContainer = ({}) => {
-  const lobbyCard = 
-}
+const CardContainer = ({ recentTopics }) => {
+  const lobbyCard = recentTopics.map((topic, index) => (
+    <LobbyCard {...topic} key={index} />
+  ));
+  return <div>{lobbyCard}</div>;
+};
 
+CardContainer.propTypes = {
+  recentTopics: PropTypes.array
+};
 
 export default CardContainer;
