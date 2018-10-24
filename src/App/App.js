b1bd91eaf5c8a6103f3c21fa591ddb06837be3be
Michael J. Lyons <mikelyons7@me.com>
2018-10-23 18:05:10 -0600

    Add eslint and cardContainer component skeleton

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import CardContainer from '../CardContainer/CardContainer';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      recentTopics: []
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hey Mike</h1>
+          <h1>Informant Lobby</h1>
         </header>
+        <CardContainer recentTopics={this.state.recentTopics} />
       </div>
     );
   }
