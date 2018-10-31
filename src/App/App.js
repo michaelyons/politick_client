03d80ad38cd4ad9a4d3582a521d975002ee4400e
Michael J. Lyons <mikelyons7@me.com>
2018-10-30 18:36:40 -0600

    Remove ugly styling from cards

--- src/App/App.js
+++ src/App/App.js
@@ -252,32 +252,6 @@ class App extends Component {
               </div>
             </div>
           </div>
-          <div className="hero-foot">
-            <nav className="tabs">
-              <div className="container">
-                <ul>
-                  <li className="is-active">
-                    <a>Overview</a>
-                  </li>
-                  <li>
-                    <a>Modifiers</a>
-                  </li>
-                  <li>
-                    <a>Grid</a>
-                  </li>
-                  <li>
-                    <a>Elements</a>
-                  </li>
-                  <li>
-                    <a>Components</a>
-                  </li>
-                  <li>
-                    <a>Layout</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </div>
         </section>
       </div>
     );
