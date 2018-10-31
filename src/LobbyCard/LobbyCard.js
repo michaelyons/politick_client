49e0571720e33a66d0f9fbd135e0049b2385ce5e
Michael J. Lyons <mikelyons7@me.com>
2018-10-31 11:06:38 -0600

    Add href to fix travis errors again

--- src/LobbyCard/LobbyCard.js
+++ src/LobbyCard/LobbyCard.js
@@ -33,7 +33,11 @@ const LobbyCard = ({
     <div className="card">
       <header className="card-header">
         <p className="card-header-title">Filing ID: {filingId}</p>
-        <a href="#" className="card-header-icon" aria-label="more options">
+        <a
+          href="http://localhost:3000/issues"
+          className="card-header-icon"
+          aria-label="more options"
+        >
           <span className="icon">
             <i className="fas fa-angle-down" aria-hidden="true" />
           </span>
