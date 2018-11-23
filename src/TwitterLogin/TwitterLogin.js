9777a18f1d17cf1a2e13d3b3e9fe297a9226a4d8
Michael J. Lyons <mikelyons7@me.com>
2018-11-23 12:40:11 -0700

    Add component when no user logged in

---
+++ src/TwitterLogin/TwitterLogin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TwitterLogin = () => {
+  return (
+    <div>
+      <a
+        href="https://ml-politick-server.herokuapp.com/twitter/login"
+        className="navbar-item"
+      >
+        LOGIN TO YOUR TWITTER <i className="fab fa-twitter" />
+      </a>
+    </div>
+  );
+};
+
+export default TwitterLogin;
