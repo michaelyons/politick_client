1a27d58d92c60feb464d25a6b1622ef43e92e038
Michael J. Lyons <mikelyons7@me.com>
2018-11-23 14:07:45 -0700

    Edit html for post tweet, still need to add on app active class

--- src/PostTweet/PostTweet.js
+++ src/PostTweet/PostTweet.js
@@ -45,19 +45,33 @@ class PostTweet extends Component {
   };
 
   render() {
+
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <textarea
+        <div className="card">
+          <header className="card-header">
+           <h2>Send Tweet</h2>
+          </header>
+          <form onSubmit={this.handleSubmit}>
+          <div className="media">
+            <div className="card-content">
+              <div className="media-content">
+              <textarea
             name="tweetText"
             rows="10"
             onChange={this.handleChange}
             value={this.state.tweetText}
           />
-          <button onClick={this.postTweet}>Send Tweet</button>
+           <button onClick={this.postTweet}>Send Tweet</button>
+              </div>
+            </div>
+          </div>
         </form>
+        </div>
+      <div>
       </div>
-    );
+      </div>
+    )
   }
 }
 
@@ -66,3 +80,46 @@ PostTweet.propTypes = {
 };
 
 export default PostTweet;
+
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './ModalCard.css';
+
+const ModalCard = ({ filingId, clientName, topic, register, date }) => {
+  return (
+    <div>
+      <div className="card">
+        <header className="card-header">
+          <p className="card-header-title">Filing ID {filingId}</p>
+          <button>
+            Tweet This Issue
+            <i className="fab fa-twitter" />
+          </button>
+          <p className="card-header-icon is-italic">Filed {date}</p>
+        </header>
+        <div className="media">
+          <div className="card-content">
+            <div className="media-content">
+              <p className="title is-size-6">{topic}</p>
+              <p className="subtitle">For Client {clientName}</p>
+              <p className="subtitle">Registered by {register}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ModalCard.propTypes = {
+  filingId: PropTypes.number,
+  topic: PropTypes.string,
+  clientName: PropTypes.string,
+  register: PropTypes.string,
+  date: PropTypes.string
+};
+
+export default ModalCard;
+
