ae3c736cd97aa054c0cfc494950ccfa439dec27b
Michael J. Lyons <mikelyons7@me.com>
2018-11-23 13:41:20 -0700

    Edit username welcome renderagain

--- src/LoginSuccess/LoginSuccess.js
+++ src/LoginSuccess/LoginSuccess.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const LoginSuccess = currentUser => {
-  console.log(currentUser);
   return (
     <div>
       <a
         href="https://ml-politick-server.herokuapp.com/twitter/logout"
         className="navbar-item"
       >
-        {`WELCOME ${currentUser}`} <i className="fab fa-twitter" />
+        {`WELCOME ${currentUser.currentUser}`} <i className="fab fa-twitter" />
       </a>
     </div>
   );
